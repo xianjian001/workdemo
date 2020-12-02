56e65deaddf07f3ab05aba1ed39477c81b637781
unknown <peicf34049@hs.handsome.com.cn>
2020-12-02 08:55:41 +0800

    refactor:bug修订

--- src/router/.invoke/router.js
+++ src/router/.invoke/router.js
@@ -5,26 +5,41 @@ export const routes = [{
     component: () => import('@/views/commodityDetails/index.vue'),
     name: 'commodityDetails',
     path: '/commodityDetails',
+    meta: {
+      keepAlive: true
+    }
   },
   {
     component: () => import('@/views/discover/index.vue'),
     name: 'discover',
     path: '/discover',
+    meta: {
+      keepAlive: true
+    }
   },
   {
     component: () => import('@/views/home/index.vue'),
     name: 'home',
     path: '/home',
+    meta: {
+      keepAlive: true
+    }
   },
   {
     component: () => import('@/views/shoppingCar/index.vue'),
     name: 'shoppingCar',
     path: '/shoppingCar',
+    meta: {
+      keepAlive: true
+    }
   },
   {
     component: () => import('@/views/wedding/index.vue'),
     name: 'wedding',
     path: '/wedding',
+    meta: {
+      keepAlive: true
+    }
   },
   {
     path: '/',
